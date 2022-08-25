f7765aa8c77f5b741f4ddd8b91fddf33654dff3f
Colby Pate <cbpate08@gmail.com>
2022-08-25 13:49:49 -0500

    Add project files.

---
+++ BankEncapsulation/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace BankEncapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myAccount = new BankAccount();
+            Console.WriteLine("How much would you like to deposit?");
+            var amountToDeposit = double.Parse(Console.ReadLine());
+            myAccount.Deposit(amountToDeposit);
+            Console.WriteLine($"Thnak you. Your new balance is ${myAccount.GetBalance()}");
+        }
+    }
+}
